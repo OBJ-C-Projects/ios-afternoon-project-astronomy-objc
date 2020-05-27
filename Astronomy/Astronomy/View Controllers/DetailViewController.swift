b0bf348508dad99b76f2da66785e1b26ca301212
fritzgt <tuanis@live.com>
2020-05-27 15:12:53 +0300

    added outlets and actions

--- Astronomy/Astronomy/View Controllers/DetailViewController.swift
+++ Astronomy/Astronomy/View Controllers/DetailViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    //MARK: - Properties
+    
+    
+    //MARK: - Outlets
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var cameraDetailLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    //MARK: - Actions
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+    }
+    
 
 }
 
